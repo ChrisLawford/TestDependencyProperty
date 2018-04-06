57213329531bdb6102c15e11b8d44a1bfc88fd08
OXTS\clawford <clawford@oxts.com>
2018-04-06 14:56:49 +0100

    Initial Commit

---
+++ TestDependencyProperty/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDependencyProperty
+{
+    class MainWindowViewModel:ObservableObject
+    {
+
+        private double m_height;
+
+        public double Height { get { return m_height; } set {m_height = value; OnPropertyChanged("Height"); } }
+
+        private double m_width;
+
+        public double Width { get { return m_width; } set { m_width = value; OnPropertyChanged("Width"); } }
+    }
+}
